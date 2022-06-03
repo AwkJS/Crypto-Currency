070162d9771cf1fbfa9592815f7cd70261ba5760
AwkJS <gborges.js@gmail.com>
2022-06-02 22:49:43 -0300

    GET Crypto Price

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 //https://www.coingecko.com/pt/api/documentation
+//https://docs.google.com/spreadsheets/d/1wTTuxXt8n9q7C4NDXqQpI3wpKu1_5bGVmP9Xz0XGSyU/edit#gid=0
 
 const api = axios.create({
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -4,8 +4,10 @@ import * as H from './styles'
 
 const Home: React.FC = () => {
 
-    const [listCrypto, setListCrypto] = useState();
+    const [priceBitcoin, setPriceBitcoin] = useState('');
+    const [priceEthereum, setPriceEthereum] = useState('');
 
+    
     async function getAllCryptos(){
 
         const cryptosResponse = await api.get('coins/list', {
@@ -17,26 +19,43 @@ const Home: React.FC = () => {
 
     }
 
-    async function priceCrypto(){
+    async function getAllSupportedCurrencies(){
 
-        const priceEthResponse = await api.get('simple/price', {
-            params: {
-
-            }
-        })
+        const SupportedResponse = await api.get('simple/supported_vs_currencies');
+        console.log(SupportedResponse)
 
     }
 
-    getAllCryptos()
+    //getAllCryptos()
+    //getAllSupportedCurrencies()
+
+    async function pricesCrypto(){
+
+        const priceResponse = await api.get('simple/price', {
+            params: {
+                ids: "bitcoin,ethereum",
+                vs_currencies: "usd",
+                include_market_cap: true,
+                include_24hr_vol: true,
+                include_24hr_change :true,
+                include_last_updated_at: true,
+            }
+        })
 
 
+        setPriceBitcoin(priceResponse.data.bitcoin.usd)
+        setPriceEthereum(priceResponse.data.ethereum.usd)
+    }
+
+    pricesCrypto()
 
     return (
 
         <>
 
             <H.GlobalContainer>
-                <h1></h1>
+                <h1>{priceBitcoin}</h1>
+                <h1>{priceEthereum}</h1>
             </H.GlobalContainer>
             
         </>
